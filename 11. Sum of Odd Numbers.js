2a082cea632ff43b640785ba201bd11923e68d43
Miroslav Tsarev <mtzarev@gmail.com>
2024-08-13 22:26:59 +0300

    Add files via upload

---
+++ 11. Sum of Odd Numbers.js
@@ -0,0 +1,19 @@
+function sumOfOddNumbers(num) {
+    let sum = 0;
+    let curValue = 0;
+    let countNum = 0;
+
+    while (countNum != num) {
+        if (curValue % 2 !== 0) {
+            console.log(curValue);
+            countNum++;
+            sum += curValue;
+        };
+        
+        curValue++;
+        
+    };
+
+    console.log(`Sum: ${sum}`);
+};
+sumOfOddNumbers(5);
